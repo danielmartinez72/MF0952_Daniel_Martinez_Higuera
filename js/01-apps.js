f0bbc207ac7f9637f4ef49c3874da92ba21a6b30
Daniel <Iguanasapiens13@gmail.com>
2023-06-20 09:50:33 +0200

    añadiendo las imagenes

---
+++ js/01-apps.js
@@ -0,0 +1,14 @@
+//string o cadena de caracteres
+//Existen 3 fromas de crearlas
+//cadena de primitiva
+const producto = 'Monitor 20 pulgadas';
+const producto2 = String('Raton gamer');
+console.log(typeof producto2);
+
+//Forma menos común
+const producto3 = new String('Teclado expecial con leds RGB');
+console.log(typeof producto3);
+
+//Escapar caracteres
+const producto4 = "'Monitor \t de 24\""
+console.log(producto4);
